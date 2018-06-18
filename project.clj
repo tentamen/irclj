caa19acec43368e4d02692cf1221b8bb10f5adf8
Quan Nguyen <huu2uan@gmail.com>
2018-06-18 21:47:56 +0700

    Deploy to clojars

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject irclj "0.5.0-alpha4"
+(defproject quan/irclj "0.5.0-alpha5"
   :description "A simple IRC library/bot framework."
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/flatland/irclj"
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :java-source-paths ["java/src"])
+  :java-source-paths ["java/src"]
+  :deploy-repositories [["clojars" {:sign-releases false}]])
